a2370cde5e5022c5c913fb61d4b37feeff14115c
JianTing <jason717011@eip.chuanhwa.com.tw>
2022-12-06 21:27:43 +0800

    first

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link to="/pinia">
+        pinia
+      </nuxt-link>
+    </div>
+    <div>
+      <nuxt-link to="/taiwind">
+        taiwind
+      </nuxt-link>
+    </div>
+    <div>
+      <nuxt-link to="/_tailwind/">
+        taiwind模板
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
